2d2f2714c1e9a8685650fb809273796ebf773d59
kongr45gpen <electrovesta@gmail.com>
2021-04-04 15:44:21 +0300

    Improve considerations for redundancy in MTTR=0 analysis

--- analysis/comparisons.jl
+++ analysis/comparisons.jl
@@ -12,22 +12,26 @@ plot(
     R,
     [reliability_single, reliability_dr, reliability_tmr],
     label = ["Single component" "2 redundancy" "3ple modular redundancy"],
-    title = "MTTR = ∞",
+    title = "MTTR = infty",
     legend = :bottomright,
     xlabel = "Reliability",
+    lw = 2
 )
 
+##
 λ = LinRange(0.0001, 0.01, 1000);
 
 λ_single = λ;
-λ_dr = @. max(-lambertw(exp(-2λ) * λ^2), 0.000000001)
-λ_tmr = λ;
+λ_dr = @. -lambertw(-sqrt(exp(-λ)) * sqrt(λ));
+λ_tmr = @. -lambertw(- exp(-2λ) * λ^2);
 
 plot(
-    R,
-    [reliability_single, reliability_dr, reliability_tmr],
-    label = ["Single component" "2 redundancy" "3ple modular redundancy"],
-    title = "MTTR = ∞",
+    λ,
+    [λ_single, λ_single, λ_dr, λ_tmr],
+    label = ["Single component" "2 redundancy, cold" "2 redundancy, hot" "3ple modular redundancy"],
+    title = "MTTR = 0",
     legend = :bottomright,
-    xlabel = "Reliability",
+    xlabel = "Failure rate",
+    yaxis = :log,
+    lw = 2
 )
