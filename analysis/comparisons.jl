e695a3ca006a19a4d8b415ac8c86f1d16874c856
kongr45gpen <electrovesta@gmail.com>
2021-07-13 21:16:27 +0300

    Update cold redundancy on permanent errors

--- analysis/comparisons.jl
+++ analysis/comparisons.jl
@@ -14,13 +14,14 @@ R = 0:0.01:1;
 ## No-repair reliability
 reliability_single = R;
 reliability_dr = @. 1 - (1 - R)^2;
+reliability_cold = @.  R * (1 - log(R));
 reliability_3r = @. 1 - (1 - R)^3;
 reliability_tmr = @. 3 * R^2 - 2 * R^3
 
 plot(
     R,
-    [reliability_single, reliability_dr, reliability_tmr, reliability_3r],
-    label = ["Single component" "Dual redundancy" "Triple modular redundancy (naive)" "Triple modular redundancy (resilient)"],
+    [reliability_single, reliability_cold, reliability_dr, reliability_tmr, reliability_3r],
+    label = ["Single component" "Dual redundancy (cold)" "Dual redundancy (hot)" "Triple modular redundancy (naive)" "Triple modular redundancy (resilient)"],
     # title = "Resiliency to permanent failures",
     legend = :outerright,
     xlabel = "Component reliability",
@@ -41,7 +42,7 @@ heatmap(
     ylabel = "Voter reliability Rv",
     colorbar_title = "System reliability",
     aspect_ratio = 1,
-    c = :inferno
+    c = :inferno,
 )
 savefig("reliability_norepair_voter_en.pdf")
 
@@ -49,25 +50,30 @@ savefig("reliability_norepair_voter_en.pdf")
 λ = 10 .^ (LinRange(-11, -2, 1000))
 
 λ_single = λ;
-λ_dr = @. -lambertw(- (2λ * exp(-λ) - λ^2 * exp(-2λ)));
-λ_tmr = @. -lambertw(- (3(λ * exp(-λ))^2 - 2(λ * exp(-λ))^3));
+λ_dr = @. -lambertw(-(2λ * exp(-λ) - λ^2 * exp(-2λ)));
+λ_tmr = @. -lambertw(-(3(λ * exp(-λ))^2 - 2(λ * exp(-λ))^3));
 
 plot(
     λ,
     [λ_single, λ_single, λ_dr, λ_tmr],
-    label = permutedims([
-        "Single component"
-        "Dual redundancy, cold"
-        "Dual redundancy, hot"
-        "Triple modular redundancy"]),
+    label = permutedims(
+        [
+            "Single component"
+            "Dual redundancy, cold"
+            "Dual redundancy, hot"
+            "Triple modular redundancy"
+        ],
+    ),
     #title = "Resilience to temporary failures",
-    legend = (0.6,0.35),
+    legend = (0.6, 0.35),
     xlabel = "Component failure rate",
     ylabel = "System failure rate",
     yaxis = :log,
     xaxis = :log,
     left_margin = -150px,
     lw = 2,
-    linecolor=[palette(:tab10)[1] palette(:tab10)[1] palette(:tab10)[2] palette(:tab10)[3]]
+    linecolor = [palette(:tab10)[1] palette(:tab10)[1] palette(:tab10)[2] palette(
+        :tab10,
+    )[3]],
 )
 savefig("reliability_repair_en.pdf")
